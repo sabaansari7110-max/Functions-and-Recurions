7a5e062ffcc889e293af49dc3992b7f26f116bc9
Saba Ansari <sabaansari7110@gmail.com>
2026-02-18 22:15:02 +0530

    Add files via upload

---
+++ 01_functions.py
@@ -0,0 +1,13 @@
+def avg():
+    a=int(input("enter your number"))
+    b=int(input("enter your number"))
+    c=int(input("enter your number"))
+    average= (a+ b+ c)/3
+    print(average)
+
+avg()
+avg()
+avg()
+avg()
+avg()
+
