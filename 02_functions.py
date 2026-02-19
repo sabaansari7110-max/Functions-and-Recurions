47e5dbe57643fd66b4fcf03d73737e57443f13e7
Saba Ansari <sabaansari7110@gmail.com>
2026-02-19 13:05:56 +0530

    Add files via upload

---
+++ 02_functions.py
@@ -0,0 +1,17 @@
+#function with arguments
+
+#1st method
+def greet(name):
+    print("hello,"+" " + name)
+      
+greet("saba")
+greet("john") 
+greet("you")
+
+#2nd method
+def goodDay(name, ending):
+    print("GoodDay, " + name)
+    print(ending)
+
+goodDay("saba", "thank you")
+goodDay("you", "thank you")
