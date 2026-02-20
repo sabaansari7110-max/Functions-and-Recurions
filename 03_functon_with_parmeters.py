b3a1f17e6a03d8adb8c16ac6180499842ce13264
Saba Ansari <sabaansari7110@gmail.com>
2026-02-20 11:57:28 +0530

    Add files via upload

---
+++ 03_functon_with_parmeters.py
@@ -0,0 +1,8 @@
+#default parmeter value
+
+def goodDay(name, ending= "thank you"):   #function body
+    print(f"Good Day, {name}")    #name will be"stranger" in function body (default)
+    print(ending)
+
+goodDay("saba", "thanks")       #name will be "saba" in function body passed 
+goodDay("alif")
