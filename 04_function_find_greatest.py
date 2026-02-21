9e0c8a7aa721fbcfbeb7c8e3a1b73b5100e9a0f9
Saba Ansari <sabaansari7110@gmail.com>
2026-02-21 12:12:14 +0530

    Add files via upload

---
+++ 04_function_find_greatest.py
@@ -0,0 +1,17 @@
+#function to find greatest of three numbers 
+
+def greatest(a, b, c):
+    if(a>b and a>c):
+       return a
+    elif(b>a and b>c):
+        return b
+    else:
+        (c>b and c>a)
+        return c
+    
+
+a= 23
+b= 21
+c= 45
+
+print(greatest(a, b, c))
