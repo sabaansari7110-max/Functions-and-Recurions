15f02535085b012255922fca9e3a2856d6454a9a
Saba Ansari <sabaansari7110@gmail.com>
2026-02-22 11:24:29 +0530

    Update 05_functions_convert_celsius-to-fahrenheit.py

--- 05_functions_convert_celsius-to-fahrenheit.py
+++ 05_functions_convert_celsius-to-fahrenheit.py
@@ -1,5 +1,4 @@
 #program using functions to convert celsius to fahrenheit
-
 # c/5 = f-32/9  (formula to covert c to f)
 
 #1st method-
@@ -16,4 +15,5 @@ def f_to_c(f):
 
 f= int(input("enter temp in f: "))
 c= f_to_c(f)
+
 print(f"{round (c, 2)}*c")
